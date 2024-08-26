d3e18796d86856e7e78558e3fe7cce1e7352d72f
GopeshShukla <160418032+GopeshShukla@users.noreply.github.com>
2024-08-26 17:02:35 +0530

    Create OTP Generator.py

---
+++ OTP Generator.py
@@ -0,0 +1,19 @@
+# Password generator
+import random   # this is module or packages
+
+import string    # this is module or packages
+
+OTP_len=6
+charValues = string.digits
+
+ # by list compression
+
+# password="".join([random.choice(charValues) for i in range(OTP_len)])
+
+#by loop
+password=""
+for i in range(OTP_len):
+  password += random.choice(charValues)
+
+
+print("Your OTP Is :-", password)
